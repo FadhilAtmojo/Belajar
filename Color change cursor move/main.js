cd781edf921831f7e38f98f52a862c6e6c16b239
Fadhill Atmojo <74691569+Fadhillatmojo@users.noreply.github.com>
2023-01-27 15:42:23 +0700

    Cursor Move Color Changes
    It is a project that can change the background color randomly when you move your cursor inside the body of browser.

---
+++ Color change cursor move/main.js
@@ -0,0 +1,6 @@
+
+document.body.addEventListener('mousemove', function (event) {
+    const xpos = Math.round((event.clientX / window.innerWidth) * 255);
+    const ypos = Math.round((event.clientY / window.innerHeight) * 255);
+    document.body.style.backgroundColor = 'rgb('+ xpos +','+ ypos +',100)';
+})
